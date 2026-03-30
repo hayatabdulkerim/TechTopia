278cd6a818d1e4df2e9a7044a57c53bd24b4a4d7
hayatabdulkerim <hayatabdulkerim11@gmail.com>
2026-03-30 11:55:27 +0300

    added bootstrap

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,13 @@
-import './App.css'
+//  Importing Bootstrap CSS & JS
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "./App.css";
+
 
 function App() {
 
   return (
-    <>
-    heyyy</>
+    <>heyy</>
   )
 }
 
