836b9b1b12df454bffa40c627ae042021d72fe24
hayatabdulkerim <hayatabdulkerim11@gmail.com>
2026-03-31 09:03:35 +0300

    initial product model setup

---
+++ server/models/productModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true, // to remove extra spaces
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0, // preventing negative prices
+    },
+    imageLink: {
+      type: String,
+      required: true,
+      match: [/^https?:\/\/.+/, "Please use a valid URL"], //This is Mongoose validation using a Regular Expression (Regex).
+    },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Product", productSchema)
+
+
+// handle favorites
