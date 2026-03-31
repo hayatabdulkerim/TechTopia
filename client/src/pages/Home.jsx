23f4c87cdfd1e01b7809a3d7ec48609ed5be6277
hayatabdulkerim <hayatabdulkerim11@gmail.com>
2026-03-31 17:21:39 +0300

    initial form setup

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,12 @@
 // Importing components
 import Navbar from "../components/Navbar"
-
+import ProductForm from '../components/ProductForm'
 export default function Home() {
   return (
     <>
-    Home 
-    <Navbar/>
     
+    <Navbar/>
+    <ProductForm/>
     </>
   )
 }
