b790db3e5138272895a2d4dcef36b416f3604c47
hayatabdulkerim <hayatabdulkerim11@gmail.com>
2026-04-04 15:42:05 +0300

    added product sections with catagories

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,21 +1,32 @@
+import { BrowserRouter, Routes, Route} from "react-router-dom";
 // Importing Bootstrap CSS & JS
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
 // Importing custome css
 import "./App.css";
 
 // Importing pages
-import Home from './pages/Home';
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
+import CategoryPage from "./pages/CategoryPage";
 
 
 function App() {
-
   return (
     <>
-    
-    <Home/>
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/category/:name" element={<CategoryPage />} />
+            {/* <Route path="/login" element={<Login />} /> */}
+            {/* <Route path="/signup" element={<Signup />} /> */}
+          </Routes>
+        </div>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
