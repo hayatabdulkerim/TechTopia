2e649d14d6c12eb1468458d8665dc2c9d9a1fdc8
hayatabdulkerim <hayatabdulkerim11@gmail.com>
2026-04-06 09:16:48 +0300

    persistant cart with local storage for gest cart

--- client/src/context/CartContext.jsx
+++ client/src/context/CartContext.jsx
@@ -1,14 +1,16 @@
-import { createContext, useReducer } from "react";
+
+import { createContext, useReducer, useEffect } from "react";
 
 export const CartContext = createContext();
 
 export const cartReducer = (state, action) => {
   switch (action.type) {
-    case "SET_CART_ITEMS":  // load all the cart items
+    case "SET_CART_ITEMS":
       return {
         cartItems: action.payload,
       };
-    case "ADD_CART_ITEM":  // called when we click add cart item
+
+    case "ADD_CART_ITEM":
       const existing = state.cartItems.find(
         (item) => item._id === action.payload._id,
       );
@@ -24,6 +26,7 @@ export const cartReducer = (state, action) => {
       return {
         cartItems: [{ ...action.payload, quantity: 1 }, ...state.cartItems],
       };
+
     case "DECREMENT_CART_ITEM":
       return {
         cartItems: state.cartItems
@@ -32,21 +35,38 @@ export const cartReducer = (state, action) => {
               ? { ...item, quantity: item.quantity - 1 }
               : item,
           )
-          .filter((item) => item.quantity > 0), // to avoid decreasing into a negative value and just removing the ones whose quantity reached 0.
+          .filter((item) => item.quantity > 0),
       };
+
     case "DELETE_CART_ITEM":
       return {
         cartItems: state.cartItems.filter(
           (item) => item._id !== action.payload._id,
         ),
       };
+
     default:
       return state;
   }
 };
 
 export const CartContextProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(cartReducer, { cartItems: [] });
+
+  const [state, dispatch] = useReducer(cartReducer, { cartItems: [] }, () => {    // the 3rd argument is called an Initializer function what it does is : “Hey React, before starting the app, check localStorage. If there’s a saved cart, use it. If not, start empty.”
+    try {
+      const storedCart = JSON.parse(localStorage.getItem("cart"));
+      return { cartItems: storedCart || [] };
+    } catch {
+      return { cartItems: [] };
+    }
+  });
+
+
+  //  save cart on localstorage whenever it changes
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(state.cartItems));
+  }, [state.cartItems]);
+
   return (
     <CartContext.Provider value={{ ...state, dispatch }}>
       {children}
